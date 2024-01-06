f224adb0cc97c930ee245ffd148027916e60eebe
Prayoga Boedihartoyo <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2024-01-06 20:26:50 +0700

    Initial Commit

--- manage_classroom/urls.py
+++ manage_classroom/urls.py
@@ -1,55 +1,10 @@
 from django.urls import path
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import login
-from django.contrib import messages
-
-
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, 'Account created successfully!')
-            return render(request, '../templates/home.html')
-        else:
-            messages.error(request, 'Error creating account. Please correct the errors below.')
-    else:
-        form = UserCreationForm()
-    return render(request, '../templates/signup.html', {'form': form})
-
-
-def signin(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-
-            if username == 'admin' and password == 'admin':
-                user = form.get_user()
-                login(request, user)
-                messages.success(request, 'Login successful!')
-                return redirect('home')
-            else:
-                messages.error(request, 'Incorrect username or password. Please try again.')
-        else:
-            messages.error(request, 'Error logging in. Please check your credentials.')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
-
-
-def dashboard(request):
-    return render(request, '../templates/dashboard.html')
-
-
-def home(request):
-    return render(request, '../templates/home.html')
+from django.contrib import admin
+from .views import signup, signin, dashboard, home
 
 
 urlpatterns = [
+    path('admin', admin.site.urls),
     path('', dashboard, name='dashboard'),
     path('login', signin, name='login'),
     path('signup', signup, name='signup'),
--- manage_classroom/views.py
+++ manage_classroom/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login
 from django.contrib import messages
@@ -19,17 +19,30 @@ def signup(request):
     return render(request, '../templates/signup.html', {'form': form})
 
 
-def loginn(request):
+def signin(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return render(request, '../templates/home.html')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+
+            if username == 'admin' and password == 'admin':
+                user = form.get_user()
+                login(request, user)
+                messages.success(request, 'Login successful!')
+                return redirect('home')
+            else:
+                messages.error(request, 'Incorrect username or password. Please try again.')
+        else:
+            messages.error(request, 'Error logging in. Please check your credentials.')
     else:
         form = AuthenticationForm()
-    return render(request, '../templates/login.html', {'form': form})
+    return render(request, 'login.html', {'form': form})
 
 
 def dashboard(request):
     return render(request, '../templates/dashboard.html')
+
+
+def home(request):
+    return render(request, '../templates/home.html')
