9947e47e332dfa8ee6495249818921d110acc30d
Prayoga Boedihartoyo <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2024-01-07 15:13:28 +0700

    Initial Commit

--- app/models.py
+++ app/models.py
@@ -1,9 +1,28 @@
+# classrooms/models.py
+
 from django.db import models
 
 
-class Classroom(models.Model):
-    teacher = models.CharField(max_length=255)
-    student = models.TextField(null=True, blank=True)
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
 
     def __str__(self):
-        return self.teacher
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Classroom(models.Model):
+    name = models.CharField(max_length=100)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student)
+
+    def __str__(self):
+        return self.name
