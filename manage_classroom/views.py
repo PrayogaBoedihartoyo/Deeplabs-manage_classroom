cfd481511e5863ef550a47439e6bff1e637fe400
Prayoga Boedihartoyo <112841946+PrayogaBoedihartoyo28@users.noreply.github.com>
2024-01-07 10:46:33 +0700

    Initial Commit

--- manage_classroom/views.py
+++ manage_classroom/views.py
@@ -1,43 +1,45 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from .forms import RegisterForm
 
 
 def signup(request):
+    if request.method == 'GET':
+        form = RegisterForm()
+        return render(request, '../templates/signup.html', {'form': form})
+
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = RegisterForm(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, 'You have singed up successfully.')
             login(request, user)
-            messages.success(request, 'Account created successfully!')
-            return render(request, '../templates/home.html')
+            return redirect('posts')
         else:
-            messages.error(request, 'Error creating account. Please correct the errors below.')
-    else:
-        form = UserCreationForm()
-    return render(request, '../templates/signup.html', {'form': form})
+            return render(request, '../templates/signup.html', {'form': form})
 
 
 def signin(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
-
-            if username == 'admin' and password == 'admin':
-                user = form.get_user()
+            user = authenticate(username=username, password=password)
+            if user is not None:
                 login(request, user)
-                messages.success(request, 'Login successful!')
-                return redirect('home')
+                messages.info(request, f"Kamu sekarang login sebagai {username}.")
+                return redirect("home")
             else:
-                messages.error(request, 'Incorrect username or password. Please try again.')
+                messages.error(request, "Invalid username or password.")
         else:
-            messages.error(request, 'Error logging in. Please check your credentials.')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'login.html', {'form': form})
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="../templates/login.html", context={"login_form": form})
 
 
 def dashboard(request):
